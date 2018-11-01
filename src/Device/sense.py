73a3b243b8911fd2703f6565a3cf22890aee8e4e
KasperCK <8913247+KasperCK@users.noreply.github.com>
2018-11-01 07:38:04 +0100

    Sensehat led control

--- src/Device/sense.py
+++ src/Device/sense.py
@@ -1,19 +1,61 @@
 import time, threading
 from sense_hat import SenseHat
 
-def singleReading():
-    s = SenseHat()
-    return [s.temp, s.humidity, s.pressure]
+class Sensor:
+    def __init__(self):
+        print("Object created!") # Test
+        self.s = SenseHat()
+        self.running = True
 
-def printReadings(r):
-    print("--------------- Readings ---------------")
-    print("Temp: ", r[0])
-    print("Humi: ", r[1])
-    print("Pres: ", r[2])
-    print("----------------------------------------")
+    def singleReading():
+        return [self.s.temp, self.s.humidity, self.s.pressure]
 
-def readPeriodically(periode, handler)
-    handler(singleReading())
-    threading.Timer(periode, readPeriodically, [periode, handler]).start()
+    def readPeriodically(periode, handler):
+        if self.running:
+            handler(self.singleReading())
+            threading.Timer(periode, self.readPeriodically, [periode, handler]).start()
 
-#readPeriodically(5, printReadings)
+    def stopReading():
+        self.running = False
+
+    def setInbounds():
+        X = [0, 255, 0] 
+        O = [255, 255, 255] 
+        mark = [
+        O, O, O, O, O, O, O, O,
+        O, X, X, O, O, X, X, O,
+        O, X, X, O, O, X, X, O,
+        O, O, O, O, O, O, O, O,
+        O, X, O, O, O, O, X, O,
+        O, O, X, O, O, X, O, O,
+        O, O, O, X, X, O, O, O,
+        O, O, O, O, O, O, O, O
+        ]
+        self.s.set_pixels(mark)
+
+    def setOutofbounds():
+        X = [255, 0, 0] 
+        O = [255, 255, 255] 
+        mark = [
+        O, O, O, O, O, O, O, O,
+        O, X, X, O, O, X, X, O,
+        O, X, X, O, O, X, X, O,
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        O, O, X, X, Xs, X, O, O,
+        O, X, O, O, O, O, X, O,
+        O, O, O, O, O, O, O, O
+        ]
+        self.s.set_pixels(mark)
+
+# Test handler:
+    def printReadings(r):
+        print("--------------- Readings ---------------")
+        print("Temp: ", r[0])
+        print("Humi: ", r[1])
+        print("Pres: ", r[2])
+        print("----------------------------------------")
+
+    
+# Testing
+s1 = Sensor()
