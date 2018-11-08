bde0c3d96e62cb31498ab04da4add495c7f917a4
KasperCK <8913247+KasperCK@users.noreply.github.com>
2018-11-08 09:06:16 +0100

    updated sensehat

--- src/Device/sense.py
+++ src/Device/sense.py
@@ -1,62 +1,53 @@
 import time, threading
 from sense_hat import SenseHat
 
-class Sense:
-    def __init__(self):
-        print("Sense created!") # Test
-        self.sense = SenseHat()
-        self.running = True
+sense = SenseHat()
 
-    def singleReading():
-        return [self.sense.temp, self.sense.humidity, self.sense.pressure]
+def singleReading():
+    return [sense.temp, sense.humidity, sense.pressure]
 
-    def readPeriodically(periode, handler):
-        if self.running:
-            handler(self.singleReading())
-            threading.Timer(periode, self.readPeriodically, [periode, handler]).start()
+def readPeriodically(periode, handler):
+    handler(singleReading())
+    threading.Timer(periode, readPeriodically, [periode, handler]).start()
 
-    def stopReading():
-        self.running = False
+def setInbounds():
+    X = [0, 255, 0] 
+    O = [255, 255, 255] 
+    mark = [
+    O, O, O, O, O, O, O, O,
+    O, X, X, O, O, X, X, O,
+    O, X, X, O, O, X, X, O,
+    O, O, O, O, O, O, O, O,
+    O, X, O, O, O, O, X, O,
+    O, O, X, O, O, X, O, O,
+    O, O, O, X, X, O, O, O,
+    O, O, O, O, O, O, O, O
+    ]
+    sense.set_pixels(mark)
 
-    def setInbounds():
-        X = [0, 255, 0] 
-        O = [255, 255, 255] 
-        mark = [
-        O, O, O, O, O, O, O, O,
-        O, X, X, O, O, X, X, O,
-        O, X, X, O, O, X, X, O,
-        O, O, O, O, O, O, O, O,
-        O, X, O, O, O, O, X, O,
-        O, O, X, O, O, X, O, O,
-        O, O, O, X, X, O, O, O,
-        O, O, O, O, O, O, O, O
-        ]
-        self.sense.set_pixels(mark)
-
-    def setOutofbounds():
-        X = [255, 0, 0] 
-        O = [255, 255, 255] 
-        mark = [
-        O, O, O, O, O, O, O, O,
-        O, X, X, O, O, X, X, O,
-        O, X, X, O, O, X, X, O,
-        O, O, O, O, O, O, O, O,
-        O, O, O, O, O, O, O, O,
-        O, O, X, X, X, X, O, O,
-        O, X, O, O, O, O, X, O,
-        O, O, O, O, O, O, O, O
-        ]
-        self.sense.set_pixels(mark)
+def setOutofbounds():
+    X = [255, 0, 0] 
+    O = [255, 255, 255] 
+    mark = [
+    O, O, O, O, O, O, O, O,
+    O, X, X, O, O, X, X, O,
+    O, X, X, O, O, X, X, O,
+    O, O, O, O, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    O, O, X, X, X, X, O, O,
+    O, X, O, O, O, O, X, O,
+    O, O, O, O, O, O, O, O
+    ]
+    sense.set_pixels(mark)
 
 # Test handler:
-    def printReadings(r):
-        print("--------------- Readings ---------------")
-        print("Temp: ", r[0])
-        print("Humi: ", r[1])
-        print("Pres: ", r[2])
-        print("----------------------------------------")
+def printReadings(r):
+    print("--------------- Readings ---------------")
+    print("Temp: ", r[0])
+    print("Humi: ", r[1])
+    print("Pres: ", r[2])
+    print("----------------------------------------")
 
     
 ### Testing
-#s1 = Sensor()
-#s1.readPeriodically(5, printReadings)
+#readPeriodically(5, printReadings)
